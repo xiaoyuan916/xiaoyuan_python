5445e34064720838904a4853fc23cef30852b879
xiaoyuan916 <402973804@qq.com>
2018-11-16 14:49:14 +0800

    提交新的机器学习相关内容

---
+++ python的机器学习/ScikitLearn数据的特征抽取.py
@@ -0,0 +1,6 @@
+from sklearn.feature_extraction import DictVectorizer
+
+onehot = DictVectorizer()  # 如果结果不用toarray，请开启sparse=False
+instances = [{'city': '北京', 'temperature': 100}, {'city': '上海', 'temperature': 60}, {'city': '深圳', 'temperature': 30}]
+X = onehot.fit_transform(instances).toarray()
+print(onehot.inverse_transform(X))
